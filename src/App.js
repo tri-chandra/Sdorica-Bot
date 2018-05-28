0bf27c0fc674055327f3af66d8e6c6e1f8a6f573
tri-chandra <tri-chandra@users.noreply.github.com>
2018-05-28 14:39:35 +1000

    Install peer dependency

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import { Button, Intent } from '@blueprintjs/core'
+
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <Button intent={Intent.SUCCESS}>Click Me!</Button>
+        <Button intent={Intent.PRIMARY}>Click Me!</Button>
+        <Button intent={Intent.DANGER}>Click Me!</Button>
+        <Button intent={Intent.WARNING}>Click Me!</Button>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
       </div>
     );
   }
