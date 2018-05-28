1d6b17f0272b0cb0d21e14f27eed9183f93c7fd5
tri-chandra <tri-chandra@users.noreply.github.com>
2018-05-28 16:20:56 +1000

    Added eslint

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
-import { Button, Intent } from '@blueprintjs/core'
+import { Button, InputGroup, Intent, Colors } from '@blueprintjs/core';
 
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Button intent={Intent.SUCCESS}>Click Me!</Button>
-        <Button intent={Intent.PRIMARY}>Click Me!</Button>
-        <Button intent={Intent.DANGER}>Click Me!</Button>
-        <Button intent={Intent.WARNING}>Click Me!</Button>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="pt-app pt-dark" style={{backgroundColor: Colors.DARK_GRAY3}}>
+//         <InputGroup leftIcon="star" placeholder="Type something..." />
+//         <Button>Click Me!</Button>
+//         <Button intent={Intent.SUCCESS}>Click Me!</Button>
+//         <Button intent={Intent.PRIMARY}>Click Me!</Button>
+//         <Button intent={Intent.DANGER}>Click Me!</Button>
+//         <Button intent={Intent.WARNING}>Click Me!</Button>
+//         {/* <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p> */}
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => (
+  <div className="pt-app pt-dark" style={{ backgroundColor: Colors.DARK_GRAY3 }}>
+    <InputGroup leftIcon="star" placeholder="Type something..." />
+    <Button>Click Me!</Button>
+    <Button intent={Intent.SUCCESS}>Click Me!</Button>
+    <Button intent={Intent.PRIMARY}>Click Me!</Button>
+    <Button intent={Intent.DANGER}>Click Me!</Button>
+    <Button intent={Intent.WARNING}>Click Me!</Button>
+  </div>
+);
 
 export default App;
